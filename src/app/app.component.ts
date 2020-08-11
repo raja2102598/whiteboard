0867fb7740722099d3c61d2ef768352da6427626
raja <rajajvr>
2020-08-11 15:43:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
+import { NgWhiteboardService } from 'ng-whiteboard';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'whiteboard';
+  color = '#333333';
+  backgroundColor = '#eee';
+  size = '5px';
+  isActive = false;
+
+  constructor(
+    private toastr: ToastrService,
+    private whiteboardService: NgWhiteboardService
+  ) {}
+  onInit() {
+    this.toastr.success('Init!');
+  }
+  onClear() {
+    this.toastr.success('Clear!');
+  }
+  onUndo() {
+    this.toastr.success('Undo!');
+  }
+  onRedo() {
+    this.toastr.success('Redo!');
+  }
+  onSave() {
+    this.toastr.success('Save!');
+  }
+  onImageAded() {
+    this.toastr.success('ImageAded!');
+  }
+
+  erase() {
+    this.whiteboardService.erase();
+  }
+  setSize(size) {
+    this.size = size;
+    this.isActive = false;
+  }
+  save() {
+    this.whiteboardService.save();
+  }
+  undo() {
+    this.whiteboardService.undo();
+  }
+  redo() {
+    this.whiteboardService.redo();
+  }
+  addImage(fileInput) {
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      this.whiteboardService.addImage(reader.result);
+      fileInput.value = '';
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgWhiteboardModule } from 'ng-whiteboard';
+import { ToastrModule } from 'ngx-toastr';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgWhiteboardModule,
+    ToastrModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
